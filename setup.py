ebe70d023a26b95cbbe51960a77b3c69f6876ce3
Ketouem <ketouem@gmail.com>
2016-01-29 18:40:25 +0100

    Enhancing setup.py metadata

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@
 
 from setuptools import find_packages, setup
 
+with open('VERSION.txt', 'r') as v:
+    version = v.read().strip()
+
 with open('requirements.txt', 'r') as reqs_file:
     reqs = reqs_file.read().splitlines()
     reqs = [r for r in reqs if r]
 
 setup(
     name='fake-lock',
-    version='0.1-dev',
-    packages=find_packages(),
-    install_requires=reqs
+    version=version,
+    packages=find_packages(exclude=['tests']),
+    install_requires=reqs,
+    license='MIT'
 )
